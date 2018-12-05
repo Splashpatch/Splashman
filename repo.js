889b041569c9d6e320ba8934be94726558a1b979
SplashThpatch <SplashThpatch@gmail.com>
2018-12-05 11:03:24 +0100

    Update repo.js

--- repo.js
+++ repo.js
@@ -3,7 +3,7 @@
 	"id": "Splashman",
 	"patches": {
 		"bgm_limbo": "BGM Legend in Limbo.",
-		"bgm_rockgirl": "BGM mod with CarrotWine's 'Touhou Rockgirl' arrange albums.",
+		"bgm_rockgirl": "BGM mod using CarrotWine's 'Touhou Rockgirl' arrange albums.",
 		"bgm_rockgirl_base": "Base files for bgm_rockgirl.",
 		"bgm_sc88pro": "BGM mod using ZUN's SC-88 Pro MIDI versions, by RomantiqueTp.",
 		"bgm_sc88pro_base": "Base files for bgm_sc88pro."
